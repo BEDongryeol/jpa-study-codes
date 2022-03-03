746abf6ab2bffb21125e8535c00bb04f4631a048
BEDongryeol <ehdfuf53@gmail.com>
2022-03-03 16:57:48 +0900

    사용자 정의 메서드 추가하는 방법

---
+++ src/main/java/dialect/MyDialect.java
@@ -0,0 +1,14 @@
+package dialect;
+
+import org.hibernate.dialect.H2Dialect;
+import org.hibernate.dialect.function.StandardSQLFunction;
+import org.hibernate.type.StandardBasicTypes;
+import org.hibernate.type.StringType;
+
+public class MyDialect extends H2Dialect {
+
+    public MyDialect() {
+        registerFunction("group_concat", new StandardSQLFunction("group_concat", StandardBasicTypes.STRING));
+    }
+
+}
